565f39eaa476b98267a16931bcd56c82894ea8c2
Arthur Golubtsov <goldartt@gmail.com>
2026-05-17 04:04:13 +0300

    Hide navigation bar in android

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -16,6 +16,7 @@ kotlin {
     dependencies {
         implementation(projects.shared)
         implementation(libs.androidx.activity.compose)
+        implementation(libs.androidx.core.ktx)
     }
 }
 
--- androidApp/src/main/kotlin/com/eugenehammer/mavlinkjoystikkmp/MainActivity.kt
+++ androidApp/src/main/kotlin/com/eugenehammer/mavlinkjoystikkmp/MainActivity.kt
@@ -5,6 +5,9 @@ import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +15,11 @@ class MainActivity : ComponentActivity() {
 
         enableEdgeToEdge()
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        // Hide navigation bar
+        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+        windowInsetsController.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        windowInsetsController.hide(WindowInsetsCompat.Type.navigationBars())
 
         setContent {
             App()
